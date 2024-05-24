fcf7c79068a29d1093965d2fe18fcc5251cc6338
w1nterm00n <kordesovad@gmail.com>
2024-05-24 23:01:20 +0400

    fetched shop items from API

--- src/components/shop-page/Shop.jsx
+++ src/components/shop-page/Shop.jsx
@@ -2,8 +2,26 @@ import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
 import ShopItem from './ShopItem'
 import './Shop.css'
+import { useState, useEffect } from 'react';
+
+
+
 
 function Shop() {
+  const [itemsArray, setItemsArray] = useState([]);
+
+  //fetching all items to rendering them
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", {
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then(json=>{
+      setItemsArray(json);
+    })
+    .catch((error) => console.error(error));
+  }, []);
+
 
   return (
     <>
@@ -11,14 +29,11 @@ function Shop() {
 
       <div className='pageWrapper'>
         <div className='shopItemsContainer'>
-          <ShopItem isAdded="true"/>
-          <ShopItem isAdded="true"/>
-          <ShopItem/>
-          <ShopItem/>
-          <ShopItem/>
-          <ShopItem/>
-          <ShopItem/>
-          <ShopItem/>
+
+          {itemsArray.map((item) => {
+            return <ShopItem key={item.id} name={item.title} image={item.image}
+            cathegory={item.cathegory} description={item.description} price={item.price}/>;
+          })}
         </div>
 
         <span className='paginationButtons'>
